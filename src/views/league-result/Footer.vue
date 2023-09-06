d5e18adfff3019c815898e5a0973e5bb38dd4f8e
Khaled Javedan <khaledjavedan76@gmail.com>
2023-09-06 14:22:52 +0400

    Created the pagination layout

--- src/views/league-result/Footer.vue
+++ src/views/league-result/Footer.vue
@@ -1,15 +1,50 @@
-<script setup></script>
-<template>pagination</template>
+<script setup>
+import { ref } from 'vue'
+
+const currentPage = ref(1)
+const pageSize = ref(20)
+const small = ref(false)
+const background = ref(true)
+const disabled = ref(false)
+
+const handleSizeChange = (val) => {
+  console.log(`${val} items per page`)
+}
+const handleCurrentChange = (val) => {
+  console.log(`current page: ${val}`)
+}
+</script>
+<template>
+  <div class="pagination-wrapper">
+    <el-pagination
+      v-model:current-page="currentPage"
+      v-model:page-size="pageSize"
+      :page-sizes="[20, 30, 40]"
+      :small="small"
+      :disabled="disabled"
+      :background="background"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
+</template>
 
 <style lang="scss" scoped>
 @import '@/assets/scss/global.scss';
-.prev-result {
-  background-color: $bg-card-color;
-  border-radius: $border-radius;
-  box-shadow:
-    rgba(0, 0, 0, 0.4) 0px 2px 4px,
-    rgba(0, 0, 0, 0.3) 0px 7px 13px -3px,
-    rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
-  padding: 1.5rem 1rem;
+
+.pagination-wrapper {
+  position: relative;
+  border: solid 1px blue;
+  width: 100%;
+  margin-top: 2rem;
+  overflow-x: scroll;
+  padding: 1rem;
+  padding-top: 0;
+
+  @include tablet {
+    @include flex-centralize;
+  }
 }
 </style>
